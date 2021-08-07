341605418efc705ea7fc9bfbe9cdb354647ee6f0
shrutileena <shrutileena@gmail.com>
2021-08-07 14:15:29 +0530

    changes in login

--- app/login.js
+++ app/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, TextInput } from 'react-native';
 
 import Animated, { Easing } from 'react-native-reanimated';
 import { TapGestureHandler, State } from 'react-native-gesture-handler';
@@ -50,7 +50,7 @@ function runTiming(clock, value, dest) {
     state.position
   ]);
 }
-class MusicApp extends Component {
+class Login extends Component {
   constructor() {
     super();
 
@@ -77,6 +77,24 @@ class MusicApp extends Component {
     this.bgY = interpolateNode(this.buttonOpacity, {
       inputRange: [0, 1],
       outputRange: [-height / 3, 0],
+      extrapolate: Extrapolate.CLAMP
+    });
+
+    this.TextInputZindex = interpolateNode(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, -1],
+      extrapolate: Extrapolate.CLAMP
+    });
+
+    this.TextInputY = interpolateNode(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [0, 100],
+      extrapolate: Extrapolate.CLAMP
+    });
+
+    this.TextInputOpacity = interpolateNode(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, 0],
       extrapolate: Extrapolate.CLAMP
     });
   }
@@ -142,12 +160,41 @@ class MusicApp extends Component {
               SIGN IN WITH GOOGLE
             </Text>
           </Animated.View>
+          <Animated.View style={{
+            zIndex: this.TextInputZindex,
+            opacity: this.TextInputOpacity,
+            transform: [{translateY: this.TextInputY}],
+            height: height / 3,
+            ...StyleSheet.absoluteFill,
+            top:null,
+            justifyContent:'center'
+            }}
+          >
+
+            <TextInput
+              placeholder="EMAIL"
+              style={styles.TextInput}
+              placeholderTextColor="black"
+            />
+
+            <TextInput
+              placeholder="PASSWORD"
+              style={styles.TextInput}
+              placeholderTextColor="black"
+            />
+
+            <Animated.View style={styles.button}>
+              <Text style={{fontSize:20,
+                fontWeight:'bold'
+              }}>SIGN IN</Text>
+            </Animated.View>
+          </Animated.View>
         </View>
       </View>
     );
   }
 }
-export default MusicApp;
+export default Login;
 
 const styles = StyleSheet.create({
   container: {
@@ -163,5 +210,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginVertical: 5
-  }
+  },
+  TextInput: {
+    height:50,
+    borderRadius: 25,
+    borderWidth: 0.5,
+    marginHorizontal: 20,
+    paddingLeft: 10,
+    marginVertical: 5,
+    borderColor: 'rgba(0,0,0,0.2)'
+   }
 });
